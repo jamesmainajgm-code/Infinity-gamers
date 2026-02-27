255aca45a61ee51aa8fe3552dbd1048530711fb7
jamesmainajgm-code <jamesmainajgm@gmail.com>
2026-02-27 11:06:19 +0300

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,6 @@
+const shopBtn = document.getElementById("shopBtn");
+
+shopBtn.addEventListener("click", () => {
+    shopBtn.innerText = "Opening Store...";
+    shopBtn.style.backgroundColor = "#22c55e";
+});
